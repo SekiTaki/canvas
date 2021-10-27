306e167fb2f8e37eec77713f2148eaa99d08eee8
Takanashi Taki <beetles1994klw@gmail.com>
2021-10-28 00:59:40 +0800

    git init

---
+++ canvas.js
@@ -0,0 +1,54 @@
+window.addEventListener('load',() =>{
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    //Resizing
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    /*--
+    ctx.strokeStyle = "red";
+    ctx.lineWdith = 5 ;
+    ctx.strokeRect(100,100,200,500);
+    ctx.strokeStyle = "blue";
+    ctx.lineWdith = 5 ;
+    ctx.strokeRect(200,200,200,500);
+    --*/
+
+    /*--
+    ctx.beginPath();
+    ctx.moveTo(100,100);
+    ctx.lineTo(200,100);
+    ctx.lineTo(200,150);
+    ctx.closePath();
+    ctx.stroke();
+    --*/
+
+    //variables
+    let painting = false;
+
+    function startPosition(e){
+        painting = true;
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath();
+    }
+
+    function draw(e){
+        if(!painting)return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "red";
+
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+    //Eventlisteners
+    canvas.addEventListener("mousedown" , startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+});
