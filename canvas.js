1ab3d4b11d6e05a69ee7f05ef5dfe294e5ead33b
Takanashi Taki <beetles1994klw@gmail.com>
2021-10-28 01:40:37 +0800

    Git init

--- canvas.js
+++ canvas.js
@@ -3,7 +3,7 @@ window.addEventListener('load',() =>{
     const ctx = canvas.getContext("2d");
 
     //Resizing
-    canvas.height = window.innerHeight;
+    canvas.height = 500;
     canvas.width = window.innerWidth;
 
     /*--
