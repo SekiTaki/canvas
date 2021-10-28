8b33b5b6223845730ca6471f882f09c2d75a2be0
Takanashi Taki <beetles1994klw@gmail.com>
2021-10-28 23:04:34 +0800

    Git init

--- canvas.js
+++ canvas.js
@@ -1,6 +1,8 @@
 window.addEventListener('load',() =>{
-    const canvas = document.querySelector("#canvas");
-    const ctx = canvas.getContext("2d");
+    
+});
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 
     //Resizing
     canvas.height = 550;
@@ -26,6 +28,8 @@ window.addEventListener('load',() =>{
 
     //variables
     let painting = false;
+    let start_background_color ="white";
+    ctx.strokeStyle = "black";
 
     function startPosition(e){
         painting = true;
@@ -34,32 +38,34 @@ window.addEventListener('load',() =>{
     function finishedPosition(){
         painting = false;
         ctx.beginPath();
+        Event.preventDefault();
     }
 
     function draw(e){
         if(!painting)return;
         ctx.lineWidth = 3;
         ctx.lineCap = "round";
-        //ctx.strokeStyle = "red";
+        //ctx.strokeStyle = "black";
 
         ctx.lineTo(e.clientX, e.clientY);
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        Event.preventDefault();
+    }
+    function clear_canvas(){
+        ctx.fillStyle = start_background_color;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+    
+    function change_color(element){
+        ctx.strokeStyle = element.style.background;
     }
 
     //Eventlisteners
     canvas.addEventListener("mousedown" , startPosition);
+    canvas.addEventListener("touchstart" , startPosition);
     canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("touchmove", finishedPosition);
     canvas.addEventListener("mousemove", draw);
-
-});
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
-
-let start_background_color ="white";
-function clear_canvas(){
-    ctx.fillStyle = start_background_color;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
